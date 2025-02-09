d5361615663fb6c7d89dc1c99f6e0647be5e3ee7
Petrov <74093293+RedArmyIsStrongestAll@users.noreply.github.com>
2025-02-09 22:49:23 +0300

    - fix bd docker - fix front docker

--- database/dump.sql
+++ database/dump.sql
@@ -1,10 +1,12 @@
-CREATE DATABASE board_games_db;
+CREATE DATABASE choice_games_db;
+SET search_path TO choice_games_db, public;
 
-\c board_games_db; 
+\c choice_games_db;
 
-CREATE TABLE games (
+CREATE TABLE public.games (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
     min_players INT NOT NULL CHECK (min_players > 0),
     max_players INT NOT NULL CHECK (max_players >= min_players)
 );
+ALTER TABLE public.games OWNER TO postgres;
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'http://localhost:8000';
+export const API_BASE_URL = process.env.VUE_APP_BACKEND_URL;
